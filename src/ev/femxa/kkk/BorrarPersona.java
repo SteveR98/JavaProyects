96522f00e60e574175d0f62de42e0adecb758408
alvar_000 <alvarodava@gmail.com>
2017-01-09 15:23:34 +0100

    1.12

--- src/ev/femxa/kkk/BorrarPersona.java
+++ src/ev/femxa/kkk/BorrarPersona.java
@@ -12,8 +12,9 @@ public class BorrarPersona {
 		scanner2 = new Scanner(System.in);
 		
 		buscarNombre = scanner2.next();
+		//TODO Crear un for para que lea el nombre y si es = que el nombre pedido borrar el nombre
 		
-		for(int i = 0; i < nombresLista.length; i++)
+//		for(int i = 0; i < nombresLista.length; i++)
 		{
 			
 		}
