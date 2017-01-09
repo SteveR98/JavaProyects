5b40e2629c2d93fd3276491252c5f49559f69544
alvar_000 <alvarodava@gmail.com>
2017-01-09 11:57:40 +0100

    EJ NAVIDAD

---
+++ src/ev/femxa/kkk/BorrarPersona.java
@@ -0,0 +1,32 @@
+package ev.femxa.kkk;
+
+import java.util.Scanner;
+
+public class BorrarPersona {
+
+	public void borrarPersona ()
+	{
+		
+		String buscarNombre = null;
+		Scanner scanner2 = null;
+		scanner2 = new Scanner(System.in);
+		
+		buscarNombre = scanner2.next();
+		
+		for(int i = 0; i < nombresLista.length; i++)
+		{
+			
+		}
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+}
--- src/ev/femxa/kkk/BuscarPersona.java
+++ src/ev/femxa/kkk/BuscarPersona.java
@@ -1,4 +1,7 @@
 package ev.femxa.kkk;
+
+import java.util.Scanner;
+
 /**
  * Clase cuyo uso va a ser el de buscar
  * a una persona y mostrar si coincide el nombre
@@ -7,7 +10,23 @@ package ev.femxa.kkk;
  *
  */
 public class BuscarPersona {
-//TODO Creear metodos para buscar a una persona y compararlos con los que hay en el array
+
+	//TODO Creear metodo para buscar a una persona y compararlos con los que hay en el array
+	
+	
+	public void buscarPersona()
+	{
+		Scanner scanner3 = null;
+		scanner3 = new Scanner (System.in);
+			String nombre_buscar = null;
+		int cont = 0;
+		
+		System.out.println("Escriba el nombre que quiere buscar:");
+		nombre_buscar = scanner3.next();
+	
+	}
+	
+	
 	
 	
 	
