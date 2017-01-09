04eeda807374c1570cae98d795cc062f07c53d6c
alvar_000 <alvarodava@gmail.com>
2017-01-09 18:05:53 +0100

    Ejercicio Palindromo

---
+++ src/ej/femx/main/ejercicioPalindromo.java
@@ -0,0 +1,64 @@
+package ej.femx.main;
+import java.util.Scanner;
+/**
+ * Crear una funcion que reciba un string y que devuelva un boolean, 
+ * indicando si el string recibido es palindromo (True) o no (False).
+ * @author alvar_000
+ *
+ */
+public class ejercicioPalindromo{
+
+	public static boolean esPalindromo(String cadena) {
+
+		boolean palabraPalin = false;
+		int inicial =0;
+		int fin= (cadena.length()-1);
+		
+		while ((inicial==fin)&& (!palabraPalin))
+		{
+			if (cadena.charAt (inicial) == cadena.charAt(fin))
+			{
+			inicial ++;
+		
+			fin --;
+			}
+		
+		else palabraPalin =true;
+		}			
+		if (!palabraPalin)
+		{
+			System.out.println("La palabra es palindromo");
+		}
+		else {
+			System.out.println("La palabra no es palindromo");
+
+		}
+		
+		
+		return palabraPalin; 
+	
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		String cadena =null ;
+		
+		Scanner scanner =null;
+		scanner =new Scanner(System.in);
+		
+		System.out.println("Inroduzca una palabra para saber si es o no Palindroma: ");
+		
+		cadena =scanner.nextLine();
+		
+		esPalindromo(cadena);
+		
+		}
+		
+	}
+	
+	
+	
+	
+
