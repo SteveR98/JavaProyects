d86bffec2c5266ded47423d3704e9e46f0a31702
alvar_000 <alvarodava@gmail.com>
2017-01-10 21:28:31 +0100

    EQUALS

---
+++ src/edu/femx/val/equals/MainEquals.java
@@ -0,0 +1,23 @@
+package edu.femx.val.equals;
+
+public class MainEquals {
+public static void main(String[] args) {
+	
+	String palabra1= "ADIOS";
+	String palabra2 ="ADIOS";
+	
+	if (palabra1== palabra2)
+		if (palabra1.equals(palabra2))
+				{
+		System.out.println("PALABRAS IGUALES");
+	}
+	else{
+	
+		System.out.println("DISTINTAS");
+	}
+		
+		
+}
+	
+	
+}
